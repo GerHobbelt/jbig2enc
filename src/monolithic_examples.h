163b177b5369c41931b826b82d9f5cd660698fd0
Ger Hobbelt <ger@hobbelt.com>
2024-05-08 16:28:36 +0200

    fixes + tweaks for monolithic build mode

--- src/monolithic_examples.h
+++ src/monolithic_examples.h
@@ -7,10 +7,7 @@
 extern "C" {
 #endif
 
-extern int jbig2dec_main(int argc, const char** argv);
-extern int jbig2dec_arith_test_main(int argc, const char** argv);
-extern int jbig2dec_huffman_test_main(int argc, const char** argv);
-extern int jbig2dec_pbm2png_main(int argc, const char** argv);
+extern int jbig2enc_main(int argc, const char** argv);
 
 #ifdef __cplusplus
 }
